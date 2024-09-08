dcb9b466c1a0d791c6954b2821cc485eb4bbe1de
VDBay <azharsubhan97@gmail.com>
2024-09-08 20:29:18 +0700

    :bug: fix issue too much information

--- common/install.sh
+++ common/install.sh
@@ -1,5 +1,12 @@
 # START
 
+VINTERNALDIR=/storage/emulated/0/
+VMODPATH=$VINTERNALDIR/vdbay_modules/
+VLOGMOD=$MODPATH/autocut.log
+VLOGINT=$VMODPATH/autocut.log
+VCONFMOD=$MODPATH/autocut.conf
+VCONFINT=$VMODPATH/autocut.conf
+
 sleep 1
 ui_print " "
 ui_print "    ___    ____                                   "
@@ -24,10 +31,6 @@ ui_print " / /_/ / / /    | |/ / /_/ / /_/ / ___ |/ /       "
 ui_print "/_____/ /_/     |___/_____/_____/_/  |_/_/        "
 ui_print "                                                  "
 ui_print " "
-sleep 1
-ui_print "Optimize your device's battery life with VDBay AI Autocut Charging."
-ui_print "- Smart, automatic charging adjustments for peak performance and longevity."
-sleep 1
 
 ui_print "Installing app..."
 if pm list packages | grep -q bellavita.toast; then
@@ -38,6 +41,25 @@ else
         ui_print "Unable to install the app. Please install it manually."
     fi
 fi
+
+ui_print "Adding configuration..."
+if [ ! -e "$VMODPATH" ]; then
+    mkdir $VMODPATH
+    if [ ! -e "$VLOGINT" ]; then
+        cp -f "$VLOGMOD" "$VLOGINT"
+        echo "$(date) - Installed" >>$VLOGINT
+    fi
+    if [ ! -e "$VCONFINT" ]; then
+        cp -f "$VCONFMOD" "$VCONFINT"
+        echo "$(date) - Installed" >>$VCONFINT
+    fi
+fi
+ui_print "Thanks to:"
+ui_print "- MiAzami"
+ui_print "- Tester"
+ui_print "- Follower/Subscriber"
+ui_print "- Topjohnwu"
+ui_print "- Zackptg5"
 
 nohup am start -a android.intent.action.VIEW -d https://t.me/vdbaymodule >/dev/null 2>&1 &
 
--- service.sh
+++ service.sh
@@ -1,71 +1,63 @@
 #!/system/bin/sh
 
-# Credit to MiAzami
+VINTERNALDIR=/storage/emulated/0/
+VMODPATH=$VINTERNALDIR/vdbay_modules/
+VLOGMOD=$MODPATH/autocut.log
+VLOGINT=$VMODPATH/autocut.log
+VCONFMOD=$MODPATH/autocut.conf
+VCONFINT=$VMODPATH/autocut.conf
 
-log_message() {
-    echo "$(date) - $1" >>$MODPATH/logfile.log
+vlog() {
+    echo "$(date) - $1" >$VLOGMOD
 }
 
-show_toast_and_notification() {
-    sed -Ei "s/^description=(\[.*][[:space:]]*)?/description=[ Module active: $1 ] /g" "$MODPATH/module.prop" || {
-        log_message "Failed to update module.prop"
-    }
-    am start -a android.intent.action.MAIN -e toasttext "Module active: $1" -n bellavita.toast/.MainActivity || {
-        log_message "Failed to send toast notification"
-    }
+vshow_info() {
+    sed -Ei "s/^description=(\[.*][[:space:]]*)?/description=[ Module status: $1 ] /g" "$MODPATH/module.prop"
+    am start -a android.intent.action.MAIN -e toasttext "Module status: $1" -n bellavita.toast/.MainActivity
+    su -lp 2000 -c "cmd notification post -S bigtext -t 'Autocut Charging AI by VDBay' tag 'Module status: $1'" >/dev/null 2>&1
+    vlog "$1"
 }
 
-charging_mode() {
-    if [ $LEVEL -lt $1 ]; then
-        echo 1 >/sys/class/power_supply/charger/online || {
-            log_message "Failed to start charging"
-        }
-        show_toast_and_notification "Charging in progress."
-    elif [ $LEVEL -gt $2 ]; then
-        echo 0 >/sys/class/power_supply/charger/online || {
-            log_message "Failed to stop charging"
-        }
-        show_toast_and_notification "Charging paused."
+vmode() {
+    VCHARGE_STATUS=$(cat /sys/class/power_supply/battery/status) #"Charging", "Discharging"
+    VLEVEL=$(cat /sys/class/power_supply/battery/capacity)       #"0-100"
+
+    if [ $VLEVEL -lt $1 ] && [ "$VCHARGE_STATUS" = "Discharging" ]; then
+        echo 1 >/sys/class/power_supply/charger/online
+        vshow_info "Charging..."
+    elif [ $VLEVEL -gt $2 ] && [ "$VCHARGE_STATUS" = "Charging" ]; then
+        echo 0 >/sys/class/power_supply/charger/online
+        vshow_info "Discharging"
     fi
 }
 
 main_activity_module() {
-    PREV_CHARGING_MODE=" "
+    vshow_info "On"
+    VIS_ENABLED_PREV=-1 #first time "-1"
     while true; do
-        CHARGE_STATUS=$(cat /sys/class/power_supply/battery/status) || {
-            log_message "Failed to read charge status"
-        }
-        LEVEL=$(cat /sys/class/power_supply/battery/capacity) || {
-            log_message "Failed to read battery level"
-        }
-        SCREEN_STATE=$(dumpsys nfc | grep 'mScreenState' | cut -d'=' -f2) || {
-            log_message "Failed to get screen state"
-        }
-        IS_POWER_SAVE=$(dumpsys nfc | grep 'mIsPowerSavingModeEnabled' | cut -d'=' -f2) || {
-            log_message "Failed to get power save status"
-        }
+        VSCREEN_STATE=$(dumpsys nfc | grep 'mScreenState' | cut -d'=' -f2)               #"ON_UNLOCKED"
+        VIS_POWER_SAVE=$(dumpsys nfc | grep 'mIsPowerSavingModeEnabled' | cut -d'=' -f2) #"true", "false"
+        VIS_ENABLED=$(cat $VCONFINT)                                                     #"0", "1"
 
-        if [ "$CHARGE_STATUS" != "Charging" ]; then
-            if [ "$PREV_CHARGING_MODE" != "X" ]; then
-                show_toast_and_notification "Device is not charging."
-            fi
-
-            sleep 90
-        else
-            if [ "$SCREEN_STATE" = "ON_UNLOCKED" ] && [ "$IS_POWER_SAVE" != "true" ]; then
-                if [ "$PREV_CHARGING_MODE" != "A" ]; then
-                    charging_mode 30 80
-                fi
+        if [ "$VIS_ENABLED" = "1" ]; then
+            if [ "$VSCREEN_STATE" = "ON_UNLOCKED" ] && [ "$VIS_POWER_SAVE" != "true" ]; then
+                vmode 30 80
             else
-                if [ "$PREV_CHARGING_MODE" != "B" ]; then
-                    charging_mode 60 90
-                fi
+                vmode 80 99
             fi
-            sleep 60
+            if [ "$VIS_ENABLED" != "$VIS_ENABLED_PREV" ]; then
+                VIS_ENABLED_PREV=$VIS_ENABLED
+                vshow_info "On"
+            fi
+        else
+            if [ "$VIS_ENABLED" != "$VIS_ENABLED_PREV" ]; then
+                VIS_ENABLED_PREV=$VIS_ENABLED
+                vshow_info "Off"
+            fi
         fi
+        sleep 60
     done
 }
 
-log_message "executed"
-su -lp 2000 -c "cmd notification post -S bigtext -t 'Autocut Charging AI' tag 'Module active: Starting...'" >/dev/null 2>&1
+vshow_info "Turning On..."
 main_activity_module &
